366789f0d617aa9788c8508991758aaed251b7be
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-09-12 20:21:46 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // const [count, setCount] = useState(4)
+  const [count, setCount] = useState(() => {
+    console.log('run function')
+    return 4
+  })
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+    console.log('decrementing')
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+  )
 }
 
 export default App;
---
+++ test.js
@@ -0,0 +1,8 @@
+const arr = ["a"];
+
+
+var results = arr.filter(function(item){
+    return typeof item ==='number';  
+});
+
+console.log(results);
